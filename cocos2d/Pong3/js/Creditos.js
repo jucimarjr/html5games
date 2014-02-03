8008865f57d7eb424fe8b2cffb6dea8f3a7d2ba5
CarlosMota <camdsf_eng@hotmail.com>
2014-02-03 15:36:43 -0400

    Tela Creditos: Voltar Corrigido

--- cocos2d/Pong3/js/Creditos.js
+++ cocos2d/Pong3/js/Creditos.js
@@ -1,44 +1,28 @@
-var creditosLayer = cc.Layer.extend({
-	init:function(){
-		this._super();
-		
-		var background = cc.Director.getInstance().getWinSizeInPixels();
-        var fundo = new cc.Sprite.create("Imagens/Telas/Tela_Creditos.png");
-        fundo.setPositionX(background.width/2);
-        fundo.setPositionY(background.height/2);
-        this.addChild(fundo);
+creditsLayer = cc.Layer.extend({
+	init:function()
+    {
+        this._super();
         
-        var back = cc.MenuItemFont.create("Voltar",'voltar',this);
-        var menu = cc.Menu.create(menu);
-        menu.setPosition(90,100);
+		var btBack = cc.Sprite.create("Imagens/palavras/voltar.png");
+		var back = cc.MenuItemSprite.create(btBack, null,null, 'btBack', this);
+		back.setPositionY(20);
+		
+        var menu = cc.Menu.create(back);
+        menu.setPosition(400,20);
         this.addChild(menu);
         
         return this;
-        
-        //var bt_voltar = cc.Sprite.create("Imagens/palavras/voltar.png");
-        //var bt_voltar_selecionado = cc.Sprite.create("Imagens/palavras/voltar_selecionado.png");
-        
-        //var voltar = cc.MenuItemSprite.create(bt_voltar,null,null,'onVoltar',this);
-        
-        //var menu = cc.Menu.create(voltar);
-        //menu.setPosition(90,100);
-        //this.addChild(menu);
-        
-        //return this;
-        
-		
-	},
-	voltar:function(){
-		alert("entrou");
-		cc.Director.getInstance().replaceScene(cc.TranstionFade.create(1.2, new menuPrincipal()));
-	}
+    },
+	btBack:function(){
+    	cc.Director.getInstance().replaceScene(cc.TransitionFade.create(0.3, new menuPrincipal()));
+    }	
 });
 
-var creditos = cc.Scene.extend({
-	onEnter:function(){
-		this._super;
-		var layer = new creditosLayer();
-		layer.init();
-		this.addChild(layer);
+creditos = cc.Scene.extend({
+    onEnter:function(){
+    	this._super();
+    	var layer = new creditsLayer();
+    	layer.init();
+    	this.addChild(layer);
 	}
-})
+});
