648b4dfaee206dd0e791f36bd9015dd6de097f40
ansoli <ansoli@gmail.com>
2014-02-27 13:28:09 -0400

    [Asteroids][Cocos2D] - Trying to create a input text

--- cocos2d/asteroids/main.js
+++ cocos2d/asteroids/main.js
@@ -52,5 +52,5 @@ var cocos2dApp = cc.Application.extend({
     }
 });
 
-var myApp = new cocos2dApp(SplashInicial);
-//var myApp = new cocos2dApp(GameScene);
+//var myApp = new cocos2dApp(SplashInicial);
+var myApp = new cocos2dApp(GameScene);
--- cocos2d/asteroids/src/HighScores.js
+++ cocos2d/asteroids/src/HighScores.js
@@ -12,6 +12,15 @@ var HighScores = cc.Sprite.extend({
 		this.highScore = cc.LabelTTF.create("ENTER YOUR NAME", "SFAtarianSystem", 50);
 		this.highScore.setColor( new cc.Color3B(255, 255, 255) );
         this.highScore.setPosition(new cc.Point(screen.width/2, screen.height/2) );
-        layer.addChild(this.highScore);				
+        layer.addChild(this.highScore);		
+
+		
+		/*var input = cc.TextFieldTTF.create("<click here for input>",null, cc.TEXT_ALIGNMENT_RIGHT,"Arial", 32);
+		input.attachWithIME();
+		input.getString();*/
+		
+		
+		var input = cc.TextFieldDelegate() ;
+		layer.addChild(input);	
     }	
 });
