9e86a77cbadbbb062a41b44e2e5f2655b01c91d8
ansoli <ansoli@gmail.com>
2014-03-03 15:44:40 -0400

    [pacman1][Cocos2D] - Splash Team screen.

---
+++ cocos2d/pacman1/cocos2d.js
@@ -0,0 +1,24 @@
+(function () {
+	var d = document;
+	var c = {
+		menuType:"canvas",
+        COCOS2D_DEBUG:2, // full debug mode
+		box2d:false, // no physics in this game
+		chipmunk: false, // no chipmunk engine
+		showFPS:false, // let's show the FPS meter
+		frameRate:60, // 60 frames per second
+		tag:"gameCanvas", // id of the canvas element
+		engineDir: "../Cocos2d-html5/cocos2d/", // path to your cocos2d installation
+
+		appFiles: ['src/resource.js', 'src/SplashTeam.js', 'src/SplashGame.js']
+	};
+	
+	window.addEventListener('DOMContentLoaded', function () {
+		var s = d.createElement("script");
+		s.src = c.engineDir + "platform/jsloader.js";
+		d.body.appendChild(s);
+		s.c = c;
+		s.id = "cocos2d-html5";
+		document.ccConfig = c;
+	});
+})();
