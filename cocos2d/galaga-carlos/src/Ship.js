1db41060028809a2b046f6d118b2982354dadc3a
CarlosMota <camdsf_eng@hotmail.com>
2014-03-09 16:52:27 -0400

    [galaga][carlos]Movimento Ship

--- cocos2d/galaga-carlos/src/Ship.js
+++ cocos2d/galaga-carlos/src/Ship.js
@@ -3,7 +3,7 @@ var shootDelay = true;
 var Ship = cc.Sprite.extend({
 	//spriteFrameCache: cc.SpriteFrameCache.getInstance(),
 	animeCache: cc.AnimationCache.getInstance(),
-	
+	speed:220,
 	velocityX: 5,
 	velocityY: 5,
 	
@@ -19,13 +19,27 @@ var Ship = cc.Sprite.extend({
          this.initWithSpriteFrame(_SpriteShip);
         
 		//Posiciona a nave no centro da tela
-		this.setPosition(new cc.Point(screen.width/2, screen.height/2));
+		this.setPosition(new cc.Point(screen.width/2, 50));
         
-		//this.scheduleUpdate();
+		this.scheduleUpdate();
         //this.createAnimation();
         layer.addChild(this);
+       
 
     },
 	
+    update:function(dt){
+    	 // Keys are only enabled on the browser
+        if (sys.platform == 'browser') {
+            var pos = this.getPosition();
+            if ((LG.KEYS[cc.KEY.a] || LG.KEYS[cc.KEY.left]) && pos.x >= 0) {
+                pos.x -= dt * this.speed;
+            }
+            if ((LG.KEYS[cc.KEY.d] || LG.KEYS[cc.KEY.right]) && pos.x <= 480) {
+                pos.x += dt * this.speed;
+            }
+            this.setPosition(pos);
+        }
+    },
 	
 })
