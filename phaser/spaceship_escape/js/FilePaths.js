2232b201501859b3e2a5bd50938449547269c09a
CarlosMota <camdsf_eng@hotmail.com>
2014-04-02 22:57:57 -0400

    [SpaceShip][phase]Organização de caminho de variáveis e Tentativa de Menu

---
+++ phaser/spaceship_escape/js/FilePaths.js
@@ -0,0 +1,21 @@
+//Sprites Buttons
+var btPlay = 'assets/buttons/btPlay_147-33.png';
+var btPlaySelected = 'assets/buttons/btPlaySelected_147-33.png';
+var btHowToPlay = 'assets/buttons/btHowToPlay_262-33.png';
+var btHowToPlaySelected = 'assets/buttons/btHowToPlaySelected_262-33.png';
+var btScore = 'assets/buttons/btScore_185-33.png';
+var btScoreSelected = 'assets/buttons/btScoreSelected_147-33.png';
+var btCredits = 'assets/buttons/btCredits_262-33.png';
+var btCreditsSelected = 'assets/buttons/btCreditsSelected_262-33.png ';
+
+//SpritesSheets (png)
+var spriteSheetsImageMenu = "assets/spritesheets/Menu.png";
+var spriteSheetsImageButtons = "assets/spritesheets/buttons.png";
+
+//SpritesSheets (json)
+var spriteSheetsJsonMenu = "assets/spritesheets/Menu.json";
+var spriteSheetsJsonButtons = "assets/spritesheets/buttons.json";
+
+//audio
+var audioSplashGame = 'assets/audio/Menu8-Bit.ogg';
+var audioSplashMenu = 'assets/audio/TheForestAwakes.ogg';
