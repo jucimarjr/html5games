141d3ebb70d7b6c0182d615dbfdf0be91493901e
GiovanniRC <giovanni.rcaldeira@gmail.com>
2014-04-05 21:17:25 -0400

    Nodejs configurado

--- MultiPong/Test/robhawkes-mozilla-festival-92336f2/game.js
+++ MultiPong/Test/robhawkes-mozilla-festival-92336f2/game.js
@@ -1,1 +1,14 @@
-// Placeholder file for Node.js game server
+var util = require("util"),
+    io = require("socket.io");
+
+var socket,
+    players;
+
+function init() {
+    players = [];
+};
+
+socket = io.listen(8000);
+
+init();
+
