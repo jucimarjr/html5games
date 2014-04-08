f1904f238fe9795ac0dba8d97ee2af863869478c
ansoli <ansoli@gmail.com>
2014-04-08 17:58:51 -0400

    [Ohhman][Phaser] Trying to add the map

---
+++ phaser/ohhman/js/Game.js
@@ -0,0 +1,18 @@
+var Game = {};
+
+Game = function (game) {
+	this.game = game;
+};
+
+Game.prototype.preload = function(){
+	game.load.tilemap('background', 'assets/tilemaps/maps/background.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.image('tile', 'assets/tilemaps/tiles/map.png');
+
+};
+
+Game.prototype.create = function(){
+	var map = game.add.tilemap('background');	
+	map.addTilesetImage('map', 'tile');
+	layer = map.createLayer('map1');
+	layer.resizeWorld();
+};
