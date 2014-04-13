9f1268fe4f127e8f4856802d64a0d3d569874b98
ansoli <ansoli@gmail.com>
2014-04-13 16:36:47 -0400

    [Ohhman][Phaser] Implementing inheritance

--- phaser/ohhman/js/Blinky.js
+++ phaser/ohhman/js/Blinky.js
@@ -5,29 +5,4 @@ Blinky = function () {
 	this.direction; //LEFT, RIGHT, UP, DOWN
 };
 
-Blinky.prototype = {
-	preload : function() {
-		//Carrega o sprite do fantasminha blinky
-		game.load.image('blinky', fp_blinky);
-	},
-
-	create : function() {
-		//Adiciona o blinky na tela
-		this.sprite = game.add.sprite(game.world.width/2, game.world.height/2, 'blinky');
-		game.physics.enable(this.sprite);
-
-		//Impede que o blinky saia dos limites da tela
-		this.sprite.body.collideWorldBounds = true;
-	},
-	
-	update : function() {
-		this.moveRandomly();
-	},
-	
-	
-	//Move o blinky
-	moveRandomly : function() {
-		//Move o blinky para testar (mudar quando tiver definido o movimento dele)
-		this.sprite.x -= this.speed;
-	}
-};
+Blinky.prototype = new Ghost();
