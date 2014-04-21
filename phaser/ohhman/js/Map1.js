08438061d187d9e29324bb2f14f33f22f5268729
ansoli <ansoli@gmail.com>
2014-04-21 15:05:32 -0400

    [Ohhman][Phaser] Trying to remove balls. It was insert object layer, decision layer, but it did not work

--- phaser/ohhman/js/Map1.js
+++ phaser/ohhman/js/Map1.js
@@ -1,32 +1,33 @@
 Map1 = function () {
 	this.map = null;
 	this.tileset = null;
-	this.tileset2 = null;
 	this.layer = null;
 	this.layer2 = null;
 	this.decisionPoint = null;
+	this.ball = null;
 };
 
 Map1.prototype = {
 	preload : function() {
-		game.load.tilemap('map1', fp_map1, null, Phaser.Tilemap.TILED_JSON);
-		game.load.image('tile1', fp_tile1);
-		game.load.image('ball', fp_tlBall);
+		game.load.tilemap('map1', fp_map1, null, Phaser.Tilemap.TILED_JSON);		
+		game.load.image('fp_allTiles', fp_allTiles);	
 	},
 
 	create : function() {
 		this.map = game.add.tilemap('map1');	
 		
-		this.tileset = this.map.addTilesetImage('tile1', 'tile1');	
-		this.tileset2 = this.map.addTilesetImage('ball', 'ball');	
+		this.tileset = this.map.addTilesetImage('allTiles', 'fp_allTiles');	
 		
-		this.layer = this.map.createLayer('Wall Layer');		
-		this.layer2 = this.map.createLayer('Ball Layer');		
+		this.ball = game.add.group();
+		this.ball.enableBody = true;
+		
+		this.layer = this.map.createLayer(fp_wallLayer);		
+		this.layer2 = this.map.createLayer(fp_ballLayer, game.width, game.height, this.ball);	
 		
 		this.layer.resizeWorld();		
 
-		this.map.setCollision(1, true, 'Wall Layer');
-		this.map.setCollision(2, true, 'Ball Layer');
+		this.map.setCollision(1, true, fp_wallLayer);
+		this.map.setCollision(2, true, fp_ballLayer);
 		
 		this.decisionPoint = game.add.group();
 		this.decisionPoint.enableBody = true;		
