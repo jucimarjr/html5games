fbd3ae944304ca7f252e6aae1b0f743d3501b717
CarlosMota <camdsf_eng@hotmail.com>
2014-04-22 20:12:53 -0400

    [multimida][zi]Cenário, splash team, menu atualizados

--- phaser/zombie invaders/js/Menu.js
+++ phaser/zombie invaders/js/Menu.js
@@ -10,7 +10,8 @@ Menu = function (game) {
 
 Menu.prototype.preload = function(){
 	game.load.image('menu', screenMenu);
-	game.load.atlas('buttons', spriteSheetsImageButtons, spriteSheetsJsonButtons);	
+	//game.load.atlas('buttons', spriteSheetsImageButtons, spriteSheetsJsonButtons);
+	game.load.atlas('buttons', spriteSheetsImageButtons2, spriteSheetsJsonButtons2);
 };
 
 Menu.prototype.create = function() {
@@ -20,25 +21,25 @@ Menu.prototype.create = function() {
 
     var fadeout;
     
-    this.btnPlay = game.add.button(game.world.centerX, game.world.centerY-100, 'buttons',
+    this.btnPlay = game.add.button(game.world.centerX,game.world.centerY+50, 'buttons',
     	    function(){ this.play(); }, this);
-    	    this.btnPlay.setFrames(btPlaySelected,btPlay);
+    	    this.btnPlay.setFrames(btPlaySelecionado,btPlay);
     	    this.btnPlay.anchor.x = 0.5;
     		    		
-    this.btnHowToPlay = game.add.button(game.world.centerX, game.world.centerY-40, 'buttons',
-    	    function(){ this.howToPlay(); }, this);
-    	    this.btnHowToPlay.setFrames(btHowToPlaySelected,btHowToPlay);
-    	    this.btnHowToPlay.anchor.x = 0.5;
-    		
-    this.btnHighScores = game.add.button(game.world.centerX, game.world.centerY + 20, 'buttons',
-    	    function () { this.highScores(); }, this);
-    	    this.btnHighScores.setFrames(btScoreSelected,btScore);
-    	    this.btnHighScores.anchor.x = 0.5;
+//    this.btnHowToPlay = game.add.button(game.world.centerX, game.world.centerY-40, 'buttons',
+//    	    function(){ this.howToPlay(); }, this);
+//    	    this.btnHowToPlay.setFrames(btHowToPlaySelelecionado,btHowToPlay);
+//    	    this.btnHowToPlay.anchor.x = 0.5;
+//    		
+//    this.btnHighScores = game.add.button(game.world.centerX, game.world.centerY + 20, 'buttons',
+//    	    function () { this.highScores(); }, this);
+//    	    this.btnHighScores.setFrames(btScoreSelected,btScore);
+//    	    this.btnHighScores.anchor.x = 0.5;
 
-    this.btnCredits = game.add.button(game.world.centerX, game.world.centerY + 80, 'buttons',
-    	    function(){ this.credits(); }, this);
-    	    this.btnCredits.setFrames(btCreditsSelected,btCredits);
-    	    this.btnCredits.anchor.x = 0.5;
+//    this.btnCredits = game.add.button(game.world.centerX, game.world.centerY + 80, 'buttons',
+//    	    function(){ this.credits(); }, this);
+//    	    this.btnCredits.setFrames(btCreditsSelec,btCredits);
+//    	    this.btnCredits.anchor.x = 0.5;
    
 };
 
@@ -50,30 +51,30 @@ Menu.prototype.play = function () {
     });
 };
 
-Menu.prototype.howToPlay = function() {
-    this.fadeOut();
-    fadeout.onComplete.add(function () {
-        //this.game.state.start('HowToPlay', HowToPlay);
-    });
-}
+//Menu.prototype.howToPlay = function() {
+//    this.fadeOut();
+//    fadeout.onComplete.add(function () {
+//        //this.game.state.start('HowToPlay', HowToPlay);
+//    });
+//}
+//
+//Menu.prototype.highScores = function () {
+//    this.fadeOut();
+//    fadeout.onComplete.add(function () {
+//        //this.game.state.start('HighScore', HighScore);
+//    });
+//}
 
-Menu.prototype.highScores = function () {
-    this.fadeOut();
-    fadeout.onComplete.add(function () {
-        //this.game.state.start('HighScore', HighScore);
-    });
-}
-
-Menu.prototype.credits = function () {
-    this.fadeOut();
-    fadeout.onComplete.add(function () {
-        //this.game.state.start('Credits', Credits);
-    });
-};
+//Menu.prototype.credits = function () {
+//    this.fadeOut();
+//    fadeout.onComplete.add(function () {
+//        //this.game.state.start('Credits', Credits);
+//    });
+//};
 
 Menu.prototype.fadeOut = function () {
     fadeout = game.add.tween(this.btnPlay).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
-    fadeout = game.add.tween(this.btnHowToPlay).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
-    fadeout = game.add.tween(this.btnHighScores).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
-   fadeout = game.add.tween(this.btnCredits).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
+//    fadeout = game.add.tween(this.btnHowToPlay).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
+//    fadeout = game.add.tween(this.btnHighScores).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
+  // fadeout = game.add.tween(this.btnCredits).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true);
 };
