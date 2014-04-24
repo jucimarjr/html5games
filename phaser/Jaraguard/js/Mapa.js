81a9fb2aa5e377ac966b9c513113d57d24b8fc6c
WevertonPerdigao <wevertonperdigao@gmail.com>
2014-04-24 18:56:36 -0400

    [Jaraguard][phaser] - Adicionando Fase 1 do Jaraguard sem os inimigos

--- phaser/Jaraguard/js/Mapa.js
+++ phaser/Jaraguard/js/Mapa.js
@@ -19,44 +19,43 @@ var cursors;
 Mapa.prototype.preload = function() {
 
     game.load.image('tiles', 'assets/tileset_fase_1.png', 22, 32);
-    game.load.image('tiles2', 'assets/tileset_fase_1 - Copia.png', 22, 32);
+    game.load.image('tiles2', 'assets/tileset_fase_1 - Retrovertido.png', 22, 32);
     game.load.image('nave', 'assets/phaser-ship.png');
-    game.load.tilemap('map', 'assets/TilesetFase1.json', null, Phaser.Tilemap.TILED_JSON);
+    game.load.tilemap('map', 'assets/mapa_fase_01.json', null, Phaser.Tilemap.TILED_JSON);
 };
 
 Mapa.prototype.create = function() {
     // Make the background color of the game's stage be white (#FFFFFF)
     //  game.stage.backgroundColor = '#FFB90F';
-    game.world.setBounds(0, 0, 1400, 1400);
-
+    game.world.setBounds(0, 0, 2400, 2400);
     this.map = game.add.tilemap('map');
-    this.tileset = this.map.addTilesetImage('TilesetFase1-1', 'tiles');
-    this.tileset2 = this.map.addTilesetImage('TilesetFase1-2', 'tiles2');
-  this.layer = this.map.createLayer('Camada de Tiles 1');
+    this.tileset = this.map.addTilesetImage('tileset_fase_1', 'tiles');
+    this.tileset2 = this.map.addTilesetImage('tileset_fase_1 - Retrovertido', 'tiles2');
+    this.layer = this.map.createLayer('Camada de Tiles 1');
     this.layer2 = this.map.createLayer('Camada de Colisao');
     this.layer.resizeWorld();
-  //  this.map.setCollision(302);
-    this.map.setCollision([302,301,159,290,278,156,12,11,35,181],true,'Camada de Colisao');
-    this.player = game.add.sprite(0, game.world.height/2, 'nave');
+    //  this.map.setCollision(302);
+   this.map.setCollision([12,360,1237,1131,1228,1108,118,46,21,110,46,1203,141,22,110],true,'Camada de Colisao');
+    this.player = game.add.sprite(0, game.world.height-100, 'nave');
 
 
     game.physics.enable(this.player);
     this.player.body.bounce.y = 0.1;
     this.player.body.linearDamping = 1;
     this.player.body.collideWorldBounds = true;
-    //game.camera.follow(this.player);
+    game.camera.follow(this.player);
     cursors = game.input.keyboard.createCursorKeys();
 
 
 };
 
 Mapa.prototype.update = function() {
-    game.debug.cameraInfo(game.camera, 32, 32);
+
     game.angle += 1;
     this.player.body.velocity.x = 0;
     this.player.body.velocity.y = 0;
     game.physics.arcade.collide(this.player, this.layer2, this.testeColisao, null, this);
-    game.camera.x += 1;
+    //  game.camera.x += 1;
     if (cursors.left.isDown)
     {
         this.player.body.velocity.x = -hozMove;
