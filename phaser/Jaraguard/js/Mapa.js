ca75669ed6f897674525eeda65bfc8d27e3e8e87
WevertonPerdigao <wevertonperdigao@gmail.com>
2014-05-05 16:13:18 -0400

    [Jaraguard][phaser] -Adicionando score , lives e colisões by inimigo tipo 1 FASE 1

--- phaser/Jaraguard/js/Mapa.js
+++ phaser/Jaraguard/js/Mapa.js
@@ -23,33 +23,48 @@ var cursors;
 var hozMove = 300;
 var vertMove = -300;
 var cursors;
+
+
+var scoreText;
+var score;
+var lives;
+var liveText;
+var playerVidas;
+var tiroInimigo;
+var explosoes;
+var animacaoPlayer;
+var espaco;
 Mapa.prototype.preload = function() {
 
     game.load.image('tiles', 'assets/tileset_fase_1.png', 22, 32);
     game.load.image('tiles2', 'assets/tileset_fase_1 - Retrovertido.png', 22, 32);
-    game.load.image('nave', 'assets/phaser-ship.png');
+    game.load.image('nave', 'assets/player.png');
     game.load.tilemap('map', 'assets/mapa_fase_01.json', null, Phaser.Tilemap.TILED_JSON);
     game.load.image('inimigoTipo1', 'assets/invader.png');
-    game.load.image('bullet', 'assets/tiroplayer.png');
+    game.load.image('tiroplayer', 'assets/tiroplayer.png');
+    game.load.image('tiroInimigo', 'assets/tiroInimigo.png');
     game.load.image('canhao', 'assets/canhaoplayer.png');
+    game.load.image('espaco', 'assets/espaco.png');
+    game.load.image('playerVidas', 'assets/playerVida.png');
+    game.load.spritesheet('explosao', 'assets/explosao.png', 128, 128);
+    game.load.spritesheet('animacaoPlayer', 'assets/Player45x33.png', 45, 33);
 };
 
 Mapa.prototype.create = function() {
 
     //  game.physics.startSystem(Phaser.Physics.NINJA);
+    espaco = game.add.image(0, 0, 'espaco');
+    espaco.fixedToCamera = true;
     game.physics.startSystem(Phaser.Physics.ARCADE);
     //  game.stage.backgroundColor = '#FFB90F';
     game.world.setBounds(0, 0, 2400, 2400);
+    //caracteristicas do mapa
     this.map = game.add.tilemap('map');
-
     this.tileset = this.map.addTilesetImage('tileset_fase_1', 'tiles');
     this.tileset2 = this.map.addTilesetImage('tileset_fase_1 - Retrovertido', 'tiles2');
-
     this.layer = this.map.createLayer('Camada de Tiles 1');
     this.layerColisao = this.map.createLayer('Camada de Colisao');
-
     this.layer.resizeWorld();
-
     this.map.setCollision([12, 21, 22, 46, 110, 118, 141, 360, 1108, 1131, 1237, 1228, 1203], true, 'Camada de Colisao');
 
     //inimigo
@@ -59,7 +74,7 @@ Mapa.prototype.create = function() {
     this.map.createFromObjects('Camada de Objetos 1', 1249, 'inimigoTipo1', 0, true, false, this.inimigoTipo1);
 
     //jogador = nave
-    this.player = game.add.sprite(0, game.world.height - 300, 'nave');
+    this.player = game.add.sprite(30, game.world.height - 300, 'nave');
     this.player.anchor.setTo(0.5, 0.5);
     game.physics.enable(this.player, Phaser.Physics.ARCADE);
     game.camera.follow(this.player);
@@ -68,11 +83,20 @@ Mapa.prototype.create = function() {
     this.tirosPlayer = game.add.group();
     this.tirosPlayer.enableBody = true;
     this.tirosPlayer.physicsBodyType = Phaser.Physics.ARCADE;
-    this.tirosPlayer.createMultiple(500, 'bullet');
+    this.tirosPlayer.createMultiple(500, 'tiroplayer');
     this.tirosPlayer.setAll('anchor.x', 0.5);
     this.tirosPlayer.setAll('anchor.y', 1);
     this.tirosPlayer.setAll('outOfBoundsKill', true);
 
+    // Tiro inimigo 
+    tiroInimigo = game.add.group();
+    tiroInimigo.enableBody = true;
+    tiroInimigo.physicsBodyType = Phaser.Physics.ARCADE;
+    tiroInimigo.createMultiple(30, 'tiroInimigo');
+    tiroInimigo.setAll('anchor.x', 0.5);
+    tiroInimigo.setAll('anchor.y', 1);
+    tiroInimigo.setAll('outOfBoundsKill', true);
+    tiroInimigo.setAll('checkWorldBounds', true);
     //  canhao da nossa nave
     canhao = game.add.group();
     canhao.enableBody = true;
@@ -81,6 +105,32 @@ Mapa.prototype.create = function() {
     canhao.setAll('anchor.x', 0.5);
     canhao.setAll('anchor.y', 1);
     canhao.setAll('outOfBoundsKill', true);
+
+    //  Pontuação
+    score = 0;
+    scoreText = game.add.text(30, game.world.centerY + 300, '' + score, {font: '34px Arial', fill: '#fff'});
+    // scoreText.visible = true;
+
+    // inicializar explosoes
+    explosoes = game.add.group();
+    explosoes.createMultiple(100, 'explosao');
+    explosoes.forEach(this.inicializarAnimacaoExplosao, this);
+
+    // inicializar animacaoPlayer
+    animacaoPlayer = game.add.group();
+    animacaoPlayer.createMultiple(100, 'animacaoPlayer');
+    animacaoPlayer.forEach(this.inicializarAnimacaoPlayer, this);
+
+
+    //  Vidas
+    lives = 2;
+    liveText = game.add.text(30, game.world.centerY + 300, ' x ' + lives, {font: '34px Arial', fill: '#fff'});
+    playerVidas = game.add.sprite(30, game.world.height - 300, 'playerVidas');
+    playerVidas.angle = 0;
+    playerVidas.alpha = 1;
+
+
+
 
 
     cursors = game.input.keyboard.createCursorKeys();
@@ -92,6 +142,7 @@ Mapa.prototype.create = function() {
 Mapa.prototype.update = function() {
 
     game.angle += 0.1;
+    this.poscionaPontuacao();
     //inicializa velocidade
     this.player.body.velocity.setTo(0, 0);
     //colisoes
@@ -131,12 +182,49 @@ Mapa.prototype.update = function() {
     }
 
 };
+Mapa.prototype.poscionaPontuacao = function() {
+    //posicao do score seguindo a camera
+    scoreText.x = game.camera.x + 120;
+    scoreText.y = game.camera.y + 6;
+    //posiciona imagem da nave 
+    playerVidas.x = game.camera.x + 250;
+    playerVidas.y = game.camera.y + 10;
+    //posicao das vidas alinhado  ao score 
+    liveText.x = game.camera.x + 280;
+    liveText.y = game.camera.y + 6;
+
+}
 Mapa.prototype.testeColisao = function() {
     console.log("pos colisao");
 };
 Mapa.prototype.colisaoByNaveByInimigo = function(player, inimigo) {
-
     inimigo.kill();
+
+// cria as explosao inimigo
+    var explosion = explosoes.getFirstExists(false);
+    explosion.reset(inimigo.body.x, inimigo.body.y);
+    explosion.play('explosao', 30, false, true);
+
+    // cria as explosao da nave
+    var explosion2 = explosoes.getFirstExists(false);
+    explosion2.reset(player.body.x, player.body.y);
+    explosion2.play('explosao', 30, false, true);
+    
+    //decrementa uma vida
+    this.controleVidas();
+ 
+    if (lives < 1)
+    {
+   player.kill();
+//    //    enemyBullets.callAll('kill');
+//
+//        stateText.text = " GAME OVER \n Click to restart";
+//        stateText.visible = true;
+//
+//        //the "click to restart" handler
+//        game.input.onTap.addOnce(restart, this);
+    }
+
 
 };
 Mapa.prototype.tiroCanhao = function() {
@@ -152,15 +240,23 @@ Mapa.prototype.tiroCanhao = function() {
 };
 
 Mapa.prototype.tiroNormal = function() {
+
     if (game.time.now > tiroTime)
     {
+
         //  Grab the first bullet we can from the pool
         tiro = this.tirosPlayer.getFirstExists(false);
 
+        // cria  Animacao Player
+        var animacaoplay = animacaoPlayer.getFirstExists(false);
+
+
         if (tiro)
         {
+            animacaoplay.reset(this.player.body.x + 25, this.player.body.y + 15);
+            animacaoplay.play('animacaoPlayer', 125, false, true);
             //  And fire it
-            tiro.reset(this.player.x, this.player.y + 8);
+            tiro.reset(this.player.x + 12, this.player.y + 8);
             tiro.body.velocity.x = +400;
             tiroTime = game.time.now + 200;
         }
@@ -171,5 +267,35 @@ Mapa.prototype.tiroNormal = function() {
 Mapa.prototype.colisaoBytirosPlayerByinimigoTipo1 = function(tiro, inimigoTipo1) {
     tiro.kill();
     inimigoTipo1.kill();
-
+    // cria as explosao :)
+    var explosion = explosoes.getFirstExists(false);
+    explosion.reset(inimigoTipo1.body.x, inimigoTipo1.body.y);
+    explosion.play('explosao', 30, false, true);
+    //pontuacao
+    this.pontuacao(20);
 };
+
+Mapa.prototype.inicializarAnimacaoExplosao = function(objetoAux) {
+
+    objetoAux.anchor.x = 0.5;
+    objetoAux.anchor.y = 0.5;
+    objetoAux.animations.add('explosao');
+};
+
+Mapa.prototype.inicializarAnimacaoPlayer = function(objetoAux) {
+
+    objetoAux.anchor.x = 0.5;
+    objetoAux.anchor.y = 0.5;
+    objetoAux.animations.add('animacaoPlayer');
+};
+
+Mapa.prototype.pontuacao = function(pontuacao) {
+    score += pontuacao;
+    scoreText.text = score;
+};
+
+Mapa.prototype.controleVidas = function() {
+    lives -= 1;
+    liveText.text =' x '+lives;
+};
+
--- phaser/Jaraguard/js/Splash.js
+++ phaser/Jaraguard/js/Splash.js
@@ -12,33 +12,33 @@ Splash.prototype.preload = function() {
 Splash.prototype.create = function() {
 	var fundo = game.add.sprite(0, 0, 'splashTeam');
 	fundo.alpha = 0;
-	
-	var fadein = game.add.tween(fundo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
-	setTimeout(function() {
-		var fadeout = game.add.tween(fundo).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
-		fadeout.onComplete.add(function() {
-			var music;
-			music = game.add.audio('Splash_Sound',1,false);
-
-		    music.play('',0,1,true);
-			fundo.x = game.width/2;
-			fundo.y = game.height/2;
-			fundo = game.add.sprite(0, 0, 'splashGame');
-//			fundo.anchor.setTo(0.5,0.5);
-			
-//			fundo.loadTexture('splashGame');
-//			fundo.animations.add('blink', ['title1_532-111.png', 'title2_532-111.png'], 15, true, false);
-//			fundo.animations.play('blink');
-			var fadein = game.add.tween(fundo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
-			
-			setTimeout(function() {
-				var fadeout = game.add.tween(fundo).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
-				fadeout.onComplete.add(function(){
-					console.log("menu");
-					music.pause();
-					game.state.start('menu', Menu);
-				});
-			}, 2500);
-		});
-	}, 3000);
+	game.state.start('menu', Menu);
+	//var fadein = game.add.tween(fundo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+//	setTimeout(function() {
+//		var fadeout = game.add.tween(fundo).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+//		fadeout.onComplete.add(function() {
+//			var music;
+//			music = game.add.audio('Splash_Sound',1,false);
+//
+//		    music.play('',0,1,true);
+//			fundo.x = game.width/2;
+//			fundo.y = game.height/2;
+//			fundo = game.add.sprite(0, 0, 'splashGame');
+////			fundo.anchor.setTo(0.5,0.5);
+//			
+////			fundo.loadTexture('splashGame');
+////			fundo.animations.add('blink', ['title1_532-111.png', 'title2_532-111.png'], 15, true, false);
+////			fundo.animations.play('blink');
+//			var fadein = game.add.tween(fundo).to( { alpha: 1 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+//			
+//			setTimeout(function() {
+//				var fadeout = game.add.tween(fundo).to( { alpha: 0 }, 2000, Phaser.Easing.Linear.None, true, 0, 0, true);
+//				fadeout.onComplete.add(function(){
+//					console.log("menu");
+//					music.pause();
+//					game.state.start('menu', Menu);
+//				});
+//			}, 2500);
+//		});
+//	}, 3000);
 };
