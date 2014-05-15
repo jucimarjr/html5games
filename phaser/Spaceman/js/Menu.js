71355571a70cf74a483577c66a8b40d4245e44e3
matheuspb95 <matheuspb95@gmail.com>
2014-05-15 16:46:50 -0400

    [phaser][spaceman]multiplayer local

--- phaser/Spaceman/js/Menu.js
+++ phaser/Spaceman/js/Menu.js
@@ -1,5 +1,5 @@
 var Menu = {};
-
+var players = 0;
 Menu = function (game) {
     this.game = game;
     this.btnPlay = null;
@@ -13,8 +13,8 @@ Menu = function (game) {
 Menu.prototype.preload = function(){
 
     //game.stage.backgroundColor = '#111111';
-    game.load.spritesheet('botoes', 'assets/screens/btns.png', 438, 72);
-    game.load.image('title', 'assets/screens/tiledbackground4.png');
+    game.load.spritesheet('botoes', 'assets/screens/btns.png', 600, 72);
+    game.load.image('title', 'assets/screens/menu.png');
 		
 };
 
@@ -25,19 +25,25 @@ Menu.prototype.create = function() {
     this.title = game.add.sprite(game.world.centerX,game.world.centerY, 'title');
     this.title.anchor.setTo(0.5,0.5);
     this.button = 1;
-    this.btnPlay = game.add.button(game.world.centerX + 15, game.world.centerY + 70, 'botoes',
-    function(){ this.play(); }, this);
-    this.btnPlay.setFrames(0, 0);
-    this.btnPlay.anchor.x = 0.5;
+    this.btnPlay1 = game.add.button(game.world.centerX + 15, game.world.centerY + 30, 'botoes',
+    function(){ this.play(1); }, this);
+    this.btnPlay1.setFrames(0, 0);
+    this.btnPlay1.anchor.x = 0.5;
+    
+    this.btnPlay2 = game.add.button(game.world.centerX + 15, game.world.centerY + 110, 'botoes',
+    function(){ this.play(2); }, this);
+    this.btnPlay2.setFrames(1, 1);
+    this.btnPlay2.anchor.x = 0.5;
 
-    this.btnCredits = game.add.button(game.world.centerX + 15, game.world.centerY + 150, 'botoes',
+    this.btnCredits = game.add.button(game.world.centerX + 15, game.world.centerY + 190, 'botoes',
     function(){ this.credits(); }, this);
-    this.btnCredits.setFrames(1, 1);
+    this.btnCredits.setFrames(2, 2);
     this.btnCredits.anchor.x = 0.5;
 };
 
-Menu.prototype.play = function () {
+Menu.prototype.play = function (p) {
     this.fadeOut();
+    players = p;
     fadeout.onComplete.add(function () {
     	game.state.start('game');
     });
@@ -53,8 +59,10 @@ Menu.prototype.credits = function () {
 Menu.prototype.fadeOut = function () {
 	fadeout = game.add.tween(this.title).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
 										.to({ x: 1500 }, 1, Phaser.Easing.Linear.None, true, 0, 0, true);
-    fadeout = game.add.tween(this.btnPlay).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
+    fadeout = game.add.tween(this.btnPlay1).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
     									  .to({ x: 1500 }, 1, Phaser.Easing.Linear.None, true, 0, 0, true);
+    fadeout = game.add.tween(this.btnPlay2).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
+	  .to({ x: 1500 }, 1, Phaser.Easing.Linear.None, true, 0, 0, true);
     fadeout = game.add.tween(this.btnCredits).to({ alpha: 0 }, 500, Phaser.Easing.Linear.None, true, 0, 0, true)
     										 .to({ x: 1500 }, 1, Phaser.Easing.Linear.None, true, 0, 0, true);
 };
