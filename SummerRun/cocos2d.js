07378b026d4538aa05187105f67098284de3bd27
ablima <lpsolidsnake@gmail.com>
2014-06-05 17:58:48 -0400

    [SummerRun] Adicionado versao final para SBGames

--- SummerRun/cocos2d.js
+++ SummerRun/cocos2d.js
@@ -27,7 +27,7 @@
 (function () {
     var d = document;
     var c = {
-        COCOS2D_DEBUG:0, //0 to turn debug off, 1 for basic debug, and 2 for full debug
+        COCOS2D_DEBUG:2, //0 to turn debug off, 1 for basic debug, and 2 for full debug
         box2d:true,
         chipmunk:false,
         showFPS:false,
@@ -35,7 +35,7 @@
         frameRate:60,
         renderMode:0,       //Choose of RenderMode: 0(default), 1(Canvas only), 2(WebGL only)
         tag:'gameCanvas', //the dom element to run cocos2d on
-        SingleEngineFile:'seasonsRun.js',
+        SingleEngineFile:'summerRun.js',
     };
 
     window.addEventListener('DOMContentLoaded', function () {
