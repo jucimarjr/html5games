6b99cdf2ba6b0924d6146049c61152495e956fcd
Fabio Pires Benigno <benigno.fabio@gmail.com>
2014-06-13 13:27:18 -0400

    [Phaser][Template] Menos 1 grande repetição de código

--- phaser/template/js/config/Config.js
+++ phaser/template/js/config/Config.js
@@ -10,19 +10,13 @@ var Config = {
 					|| navigator.userAgent.match(/iPad/i)
 					|| navigator.userAgent.match(/iPod/i)
 					|| navigator.userAgent.match(/BlackBerry/i)
-					|| navigator.userAgent.match(/Windows Phone/i),
+					|| navigator.userAgent.match(/Windows Phone/i) ? true : false,
 		screen: {
 			width: 960,
 			height: 600,
 			resize: function (game) {
 				"use strict";
-				if (navigator.userAgent.match(/Android/i)
-						|| navigator.userAgent.match(/webOS/i)
-						|| navigator.userAgent.match(/iPhone/i)
-						|| navigator.userAgent.match(/iPad/i)
-						|| navigator.userAgent.match(/iPod/i)
-						|| navigator.userAgent.match(/BlackBerry/i)
-						|| navigator.userAgent.match(/Windows Phone/i)) {
+				if (Config.global.isMobile) {
 					game.scale.setExactFit();
 					game.scale.refresh();
 					document.getElementById('everything').style.width = '100%';
