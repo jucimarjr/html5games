076508f5256eb7032793529674f5c12a36e67b79
Fabio Benigno <benigno.fabio@gmail.com>
2014-08-04 12:00:13 -0400

    [Phaser][Multiplayer] Trajetória das peças melhorada

--- phaser/multiplayer/js/game.js
+++ phaser/multiplayer/js/game.js
@@ -2,6 +2,71 @@
 
 var game, socket, userroom, pieces, pieceWidth, pieceHeight, myPieces, selected;
 
+
+function countCarroca(array, branch) {
+	'use strict';
+	var i, counter = 0, size = pieces[array].length;
+	for (i = 0; i < size; i = i + 1) {
+		if (pieces[array][i].side1 === pieces[array][i].side2 && pieces[array][i].branch === branch) {
+			counter = counter + 1;
+		}
+	}
+	return counter;
+}
+
+function calculateRotationMoment(array) {
+	'use strict';
+	if (pieces.grow[array] === 'top' && countCarroca(array, pieces.branch[array]) === 2) {
+		pieces.numberRotate[array][0] = pieces.numberRotate[array][0] + 1;
+	} else if (pieces.grow[array] === 'right' && countCarroca(array, pieces.branch[array]) === 1) {
+		pieces.numberRotate[array][0] = pieces.numberRotate[array][0] + 1;
+	} else if (pieces.grow[array] === 'left' && countCarroca(array, pieces.branch[array]) === 1) {
+		pieces.numberRotate[array][0] = pieces.numberRotate[array][0] + 1;
+	} else if (pieces.grow[array] === 'bottom' && countCarroca(array, pieces.branch[array]) === 2) {
+		pieces.numberRotate[array][0] = pieces.numberRotate[array][0] + 1;
+	}
+}
+
+function rotateNextPosition() {
+	'use strict';
+	var i, arrays;
+	arrays = ['top', 'right', 'bottom', 'left'];
+	for (i = 0; i < 4; i = i + 1) {
+		if (pieces.numberRotate[arrays[i]].length > 0 && pieces[arrays[i]].length === pieces.numberRotate[arrays[i]][0]) {
+			pieces.numberRotate[arrays[i]][1] += pieces.numberRotate[arrays[i]][0];
+			pieces.numberRotate[arrays[i]].shift();
+			pieces.angle[arrays[i]] = pieces.angle[arrays[i]] + 90;
+			if (pieces.grow[arrays[i]] === 'left') {
+				pieces.next[arrays[i]].x = pieces.next[arrays[i]].x + pieceHeight / 2 + pieceWidth / 2;
+				pieces.next[arrays[i]].y = pieces.next[arrays[i]].y - pieceWidth / 2 - pieceHeight / 2;
+				if (pieces[arrays[i]][pieces[arrays[i]].length - 1].side1 === pieces[arrays[i]][pieces[arrays[i]].length - 1].side2) {
+					pieces.next[arrays[i]].y = pieces.next[arrays[i]].y - pieceHeight / 4;
+				}
+			} else if (pieces.grow[arrays[i]] === 'top') {
+				pieces.next[arrays[i]].x = pieces.next[arrays[i]].x + pieceHeight / 2 + pieceWidth / 2;
+				pieces.next[arrays[i]].y = pieces.next[arrays[i]].y + pieceWidth / 2 + pieceHeight / 2;
+				if (pieces[arrays[i]][pieces[arrays[i]].length - 1].side1 === pieces[arrays[i]][pieces[arrays[i]].length - 1].side2) {
+					pieces.next[arrays[i]].x = pieces.next[arrays[i]].x + pieceHeight / 4;
+				}
+			} else if (pieces.grow[arrays[i]] === 'right') {
+				pieces.next[arrays[i]].x = pieces.next[arrays[i]].x - pieceHeight / 2 - pieceWidth / 2;
+				pieces.next[arrays[i]].y = pieces.next[arrays[i]].y + pieceWidth / 2 + pieceHeight / 2;
+				if (pieces[arrays[i]][pieces[arrays[i]].length - 1].side1 === pieces[arrays[i]][pieces[arrays[i]].length - 1].side2) {
+					pieces.next[arrays[i]].y = pieces.next[arrays[i]].y + pieceHeight / 4;
+				}
+			} else {
+				pieces.next[arrays[i]].x = pieces.next[arrays[i]].x - pieceHeight / 2 - pieceWidth / 2;
+				pieces.next[arrays[i]].y = pieces.next[arrays[i]].y - pieceWidth / 2 - pieceHeight / 2;
+				if (pieces[arrays[i]][pieces[arrays[i]].length - 1].side1 === pieces[arrays[i]][pieces[arrays[i]].length - 1].side2) {
+					pieces.next[arrays[i]].x = pieces.next[arrays[i]].x - pieceHeight / 4;
+				}
+			}
+			pieces.grow[arrays[i]] = arrays[(arrays.indexOf(pieces.grow[arrays[i]]) + 1) % arrays.length];
+			pieces.branch[arrays[i]] = pieces.branch[arrays[i]] + 1;
+		}
+	}
+}
+
 function createRectangle(objectString) {
 	'use strict';
 	var sprite, object;
@@ -11,7 +76,12 @@ function createRectangle(objectString) {
 	sprite.angle = object.piece.angle;
 	sprite.frame = object.piece.frame;
 	pieces.next = object.next;
+	pieces.angle = object.angle;
 	pieces[object.array].push(object.piece);
+	if (object.piece.side1 === object.piece.side2) {
+		calculateRotationMoment(object.array);
+	}
+	rotateNextPosition();
 }
 
 function setPiece(x, y, angle) {
@@ -33,22 +103,56 @@ function isMovePossible(piece, array) {
 
 function rotatePiece(piece, array) {
 	'use strict';
-	var last;
-	last = pieces[array].length - 1;
-	if (piece.side1 === pieces[array][last].side1 || piece.side2 === pieces[array][last].side2) {
-		piece.angle = pieces[array][last].angle + 180;
-		piece.freeSide = piece.side1 === pieces[array][last].side1 ? 'side2' : 'side 1';
-	} else if (piece.side1 === pieces[array][last].side2 || piece.side2 === pieces[array][last].side1) {
-		piece.angle = pieces[array][last].angle;
-		piece.freeSide = piece.side1 === pieces[array][last].side2 ? 'side2' : 'side 1';
+	var index;
+	index = pieces[array].length - 1;
+	if (pieces[array][index].side1 === pieces[array][index].side2) {
+		index = index - 1;
 	}
-	if (piece.side1 === piece.side2 && (piece.side1 === pieces[array][last].side1 || piece.side1 === pieces[array][last].side2)) {
-		piece.angle = pieces[array][last].angle + 90;
-	} else if (pieces[array][last].side1 === pieces[array][last].side2 && (pieces[array][last].side1 === piece.side1 || pieces[array][last].side1 === piece.side2)) {
-		if (piece.side1 === pieces[array][last - 1].side1 || piece.side2 === pieces[array][last - 1].side2) {
-			piece.angle = pieces[array][last].angle + 180;
-		} else if (piece.side1 === pieces[array][last - 1].side2 || piece.side2 === pieces[array][last - 1].side1) {
-			piece.angle = pieces[array][last].angle;
+	if (piece.side1 === piece.side2 && (piece.side1 === pieces[array][index].side1 || piece.side1 === pieces[array][index].side2)) {
+		piece.angle = pieces.angle[array] + 90;
+		piece.freeSide = 'side1';
+		return;
+	}
+	if (piece.side1 === pieces[array][index].side1 || piece.side2 === pieces[array][index].side2) {
+		piece.angle = pieces.angle[array] + 180;
+		pieces.angle[array] = piece.angle;
+		piece.freeSide = piece.side1 === pieces[array][index].side1 ? 'side2' : 'side1';
+	} else if (piece.side1 === pieces[array][index].side2 || piece.side2 === pieces[array][index].side1) {
+		piece.angle = pieces.angle[array];
+		pieces.angle[array] = piece.angle;
+		piece.freeSide = piece.side1 === pieces[array][index].side2 ? 'side2' : 'side1';
+	}
+}
+
+function calculateNextPosition(array, piece) {
+	'use strict';
+	if (pieces.grow[array] === 'left') {
+		if (piece.side1 === piece.side2) {
+			pieces.next[array].x = pieces.next[array].x - pieceWidth;
+			piece.x = piece.x + pieceHeight / 2 - pieceWidth / 2;
+		} else {
+			pieces.next[array].x -= pieceHeight;
+		}
+	} else if (pieces.grow[array] === 'top') {
+		if (piece.side1 === piece.side2) {
+			pieces.next[array].y = pieces.next[array].y - pieceWidth;
+			piece.y = piece.y + pieceHeight / 2 - pieceWidth / 2;
+		} else {
+			pieces.next[array].y -= pieceHeight;
+		}
+	} else if (pieces.grow[array] === 'right') {
+		if (piece.side1 === piece.side2) {
+			pieces.next[array].x = pieces.next[array].x + pieceWidth;
+			piece.x = piece.x - pieceHeight / 2 + pieceWidth / 2;
+		} else {
+			pieces.next[array].x += pieceHeight;
+		}
+	} else {
+		if (piece.side1 === piece.side2) {
+			pieces.next[array].y = pieces.next[array].y + pieceWidth;
+			piece.y = piece.y - pieceHeight / 2 + pieceWidth / 2;
+		} else {
+			pieces.next[array].y += pieceHeight;
 		}
 	}
 }
@@ -61,15 +165,14 @@ function playPiece2(object) {
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.bottom.y += pieceHeight;
 	} else {
 		object.piece = setPiece(pieces.next.top.x, pieces.next.top.y, pieces.next.top.angle);
 		object.array = 'top';
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.top.y -= pieceHeight;
 	}
+	calculateNextPosition(object.array, object.piece);
 	rotatePiece(object.piece, object.array);
 	object.next = pieces.next;
 	return object;
@@ -88,29 +191,26 @@ function playPiece4(object) {
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.top.y -= pieceHeight;
 	} else if ((dBottom <= dTop && dBottom <= dLeft && dBottom <= dRight)) {
 		object.piece = setPiece(pieces.next.bottom.x, pieces.next.bottom.y, pieces.next.bottom.angle);
 		object.array = 'bottom';
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.bottom.y += pieceHeight;
 	} else if ((dRight <= dTop && dRight <= dLeft && dRight <= dBottom)) {
 		object.piece = setPiece(pieces.next.right.x, pieces.next.right.y, pieces.next.right.angle);
 		object.array = 'right';
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.right.x += pieceHeight;
 	} else {
 		object.piece = setPiece(pieces.next.left.x, pieces.next.left.y, pieces.next.left.angle);
 		object.array = 'left';
 		if (!isMovePossible(object.piece, object.array)) {
 			return false;
 		}
-		pieces.next.left.x -= pieceHeight;
 	}
+	calculateNextPosition(object.array, object.piece);
 	rotatePiece(object.piece, object.array);
 	object.next = pieces.next;
 	return object;
@@ -136,6 +236,8 @@ function onClick() {
 		console.log('move refused');
 		return;
 	}
+	object.piece.branch = pieces.branch[object.array];
+	object.angle = pieces.angle;
 	socket.emit('user clicked!', JSON.stringify(object));
 }
 
@@ -205,6 +307,30 @@ function create() {
 			bottom: {x: game.world.centerX, y: game.world.centerY + (pieceHeight / 2 + pieceWidth / 2), angle: 0},
 			left: {x: game.world.centerX - pieceHeight, y: game.world.centerY, angle: 90},
 			right: {x: game.world.centerX + pieceHeight, y: game.world.centerY, angle: 90}
+		},
+		grow: {
+			top: 'top',
+			bottom: 'bottom',
+			left: 'left',
+			right: 'right'
+		},
+		angle: {
+			top: 0,
+			bottom: 180,
+			left: 270,
+			right: 90
+		},
+		numberRotate: {
+			top: [5, 6, 3, 5, 2, 3, 1],
+			bottom: [5, 6, 3, 5, 2, 3, 1],
+			left:  [7, 4, 4, 4, 3, 2, 2],
+			right:  [7, 4, 4, 4, 3, 2, 2]
+		},
+		branch: {
+			top: 0,
+			bottom: 0,
+			left: 0,
+			right: 0
 		}
 	};
 	pieces.top.push({side1: 6, side2: 7, angle: 0, freeSide: 'side1'});
