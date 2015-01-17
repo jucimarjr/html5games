f99b4db9f8805baa71019d21824ac74b3a98a183
Fabio Benigno <benigno.fabio@gmail.com>
2015-01-17 11:38:18 -0400

    [Phaser][DominoMultiplayer] Avoiding inconsistency during login
    - Changing to room state after connection stablished in login state
    - The system is not prepared to handle reconnection in login state,
    so if the server crashes after connection stablished and it is still in
    login state the system may present unexpected behavior after reconnection.
    
    Signed-off-by: Fabio Benigno <benigno.fabio@gmail.com>

--- phaser/DominoMultiplayer/client/src/js/rules.js
+++ phaser/DominoMultiplayer/client/src/js/rules.js
@@ -16,9 +16,9 @@ Rules.prototype = {
         stateMachine.addTransition(States.LOGIN, Events.LOGIN_REFUSED, States.LOGIN, [this.actions.showLoginInvalid.bind(this.actions)]);
         stateMachine.addTransition(States.LOGIN, Events.LOGIN_CONFIRMED, States.LOGIN, [this.actions.requestServerAddress.bind(this.actions)]);
         stateMachine.addTransition(States.LOGIN, Events.IP_RECEIVED, States.LOGIN, [this.actions.connectToSever.bind(this.actions)]);
-        stateMachine.addTransition(States.LOGIN, Events.CONNECTION_ESTABLISHED, States.LOGIN, [this.actions.sendLoginToServer.bind(this.actions)]);
-        stateMachine.addTransition(States.LOGIN, Events.SERVER_ACK_LOGIN, States.LOGIN, [this.actions.requestRoomsInfo.bind(this.actions)]);
-        stateMachine.addTransition(States.LOGIN, Events.ROOMS_INFO_RECEIVED, States.ROOMS, [this.actions.populateRoomsPage.bind(this.actions), this.actions.showRoomsPage.bind(this.actions)]);
+        stateMachine.addTransition(States.LOGIN, Events.CONNECTION_ESTABLISHED, States.ROOMS, [this.actions.sendLoginToServer.bind(this.actions)]);
+        stateMachine.addTransition(States.ROOMS, Events.SERVER_ACK_LOGIN, States.ROOMS, [this.actions.requestRoomsInfo.bind(this.actions)]);
+        stateMachine.addTransition(States.ROOMS, Events.ROOMS_INFO_RECEIVED, States.ROOMS, [this.actions.populateRoomsPage.bind(this.actions), this.actions.showRoomsPage.bind(this.actions)]);
         stateMachine.addTransition(States.ROOMS, Events.ROOM_CLICKED, States.ROOMS, [this.actions.requestEnterRoom.bind(this.actions)]);
         stateMachine.addTransition(States.ROOMS, Events.SERVER_ALLOW_ENTER_ROOM, States.WAIT_PLAYERS, [this.actions.showWaitMorePlayersPage.bind(this.actions)]);
         stateMachine.addTransition(States.ROOMS, Events.LOGOUT_CLICKED, States.LOGIN, [this.actions.disconnect.bind(this.actions), this.actions.showLoginPage.bind(this.actions)]);
